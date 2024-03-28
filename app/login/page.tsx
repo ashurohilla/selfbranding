e5b606176dc6ab3b6c206d11f334da256be361ba
Ashish <90093173+ashurohilla@users.noreply.github.com>
2024-03-28 23:08:33 +0530

    adding login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import Loginform from '../navbar/loginform'
+
 export default function Login({
   searchParams,
 }: {
@@ -56,7 +57,8 @@ export default function Login({
   }
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+   <div className='justify-center flex pt-[90px]'>
+     <div className="flex-1 pt-10  flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
         href="/"
         className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
@@ -77,10 +79,9 @@ export default function Login({
         </svg>{' '}
         Back
       </Link>
-
-      <Button>
-        <Link href="/login">Dashboard</Link>
-      </Button>
+      <h3 className="text-3xl font-extrabold mb-8 max-md:text-center">
+              Sign in
+            </h3>
 
       <form
         className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
@@ -105,6 +106,19 @@ export default function Login({
           placeholder="••••••••"
           required
         />
+         <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                <label htmlFor="remember-me" className="ml-3 block text-sm">
+                  Remember me
+                </label>
+              </div>
+              <div className="text-sm">
+                <a href="jajvascript:void(0);" className="text-blue-600 hover:text-blue-500">
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
         <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
           Sign In
         </button>
@@ -120,11 +134,10 @@ export default function Login({
           </p>
         )}
       </form>
+      <p className="my-4 text-sm text-gray-400 text-center">or continue with</p>
       <Loginform/>
-
-
-    
     </div>
+   </div>
   )
 }
  
